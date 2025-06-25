7f7dd68714671554a9ec67de65131f8c9eb554d9
MaichlHendi <maichl.hendi@hotmail.com>
2025-06-25 04:58:56 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,93 @@
-B.Sc theis on trustworthy autonomy for the electrical engineering departmend at KTH
+## KEX Project: Patch Attacks
+directories:
+- cfg: contains cfg files for possible object detection victim models (YOLOv2 by default)
+- checkpoints: general folder for other model weights (for now it contains the weights for ResNet50 trained for CIFAR-10)
+- data: contains a small amount of examples for public benchmark datasets for object detection (INRIA and Pascal VOC) and image classification (Imagenet and CIFAR-10)
+- nets: torch model for the image classification victim model (ResNet50)
+- patches: png/jpg images of trained patches ready to be applied
+- utils: contains utils.py, a file with helper functions of object detection
+- weights: the weights for YOLOv2 should be placed in this folder (IMPORTANT! this weight file is not in the repository due to file size)
 
-Credit: https://github.com/gerkbyrd/KEX_PatchAttacks/tree/main
+files:
+- cfg.py, darknet.py, region_loss.py: files required to run YOLOv2 using PyTorch.
+- helper.py: various helper functions to perform object detection.
+- patch_folder_creator.py, patch_folder_creator_ic.py: code to apply already trained patches (saved in the "patches" folder) on clean images pertaining to object detection and image classification.
+- load_data.py, median_pool.py: files required to run the code to train/apply patch attacks.
+- PatchAttacker.py: basic file to run non-universal attacks on image classification (may be irrelevant to the project...)
+
+This code is based on the following publicly available repositories:
+
+- https://github.com/Zhang-Jack/adversarial_yolo2
+- https://github.com/inspire-group/PatchGuard/tree/master
+
+For attacks on CIFAR-10 it is necessary to download the resnet50_192_cifar.pth file from https://github.com/inspire-group/PatchGuard/tree/master and place it in the checkpoints folder (already done!)
+
+For attacks on INRIA and Pascal VOC it is necessary to follow the instructions on https://github.com/Zhang-Jack/adversarial_yolo2 to download the yolo.weights file into the weights folder (IMPORTANT TO DO)
+
+
+## EXAMPLE COMMANDS - APPLY PATCHES
+
+Here are some examples on how to apply already trained patches from available in "patches/". For the following commands, patched version of the clean images located at the folder specified using "--imgdir FOLDER" will be saved at "FOLDER/NAME" where the NAME is provided using "--p_name NAME".
+
+Apply the "Princess (Frozen)" patch on the INRIA images:
+```
+python patch_folder_creator.py --imgdir data/inria/clean/ --n_patches 1 --patchfile patches/newpatch.PNG --p_name test/
+```
+For double patches:
+```
+python patch_folder_creator.py --imgdir data/inria/clean/ --n_patches 2 --patchfile patches/newpatch.PNG --p_name test/
+```
+Note that the last commands will apply patches on at most one object per image, one can change the max_lab argument (by default 1) to change this. For double patches applied on up to 3 objects per image (see --max_lab argument!):
+```
+python patch_folder_creator.py --imgdir data/inria/clean/ --n_patches 1 --patchfile patches/newpatch.PNG --p_name test/ --max_lab 3
+```
+
+For image classification, we have to also specify what dataset we are using and the size of the patch. We do not need to define the size for object detection because it is resized depending on the attacked object, but in image classification the patch is randomly placed somewhere in the image. Note also that the size of the patch when we input "--patch_size SIZE" is SIZE x SIZE. Hence, for 32x32 single patches on imagenet using the "Electric Guitar" patch:
+```
+patch_folder_creator_ic.py --imgdir data/imagenet/clean/ --dataset imagenet --n_patches 1 --patchfile patches/imgnet_patch.PNG --p_name test/ --patch_size 32
+```
+For double patches:
+```
+patch_folder_creator_ic.py --imgdir data/imagenet/clean/ --dataset imagenet --n_patches 2 --patchfile patches/imgnet_patch.PNG --p_name test/ --patch_size 32
+```
+And quadruple patches:
+```
+patch_folder_creator_ic.py --imgdir data/imagenet/clean/ --dataset imagenet --n_patches 4 --patchfile patches/imgnet_patch.PNG --p_name test/ --patch_size 32
+```
+
+Note that in all examples, the area of the single patch attack is maintained when using multiple patches, that is, multiple patches are smaller to ensure that the attacked area is the same.
+
+## EXAMPLE COMMANDS - TRAIN PATCHES
+
+Here are some examples on how to train a patch from scratch. For the following examples commands, the clean images the train patch will attack are located at the folder specified using "--imgdir FOLDER", the labels indicating objects detected in the clean images by the victim model are at the folder specified using "--labdir FOLDER". The trained patch will be saved at "FOLDER/universal_patch.png" specified provided using "--savedir FOLDER/". 
+
+Train a patch on the clean INRIA images (patience represents the number of epochs without imporvements since the best patch so far before stopping):
+```
+python train_patch.py --patience 100 --imgdir data/inria/clean --labdir data/inria/yolo-labels/ --savedir testing/
+```
+
+This should yield a patch similar to the one in "testing/test_patch.png".
+
+
+## EXAMPLE COMMANDS - DEFENSE, EVALUATION, AND VISUALIZATION
+
+Here are some examples on how evaluate the effectiveness of patch attacks and defenses (this code is built around the NutNet defense). For the following example commands, the clean images are located at the folder specified using "--imgdir FOLDER", and their attacked versions (with the same names) at the folder specified using "--patch_imgdir FOLDER". Visualziations of the obejct detection results can be saved in the folder specified by using "--savedir FOLDER/" and the flag "--visualize". See the "nutnet_results" folder for some examples of visualization. The "--clean" flag indicates that the code will run only on clean images (this is equivalent to using the clean image folder in both "--imgdir" and "--patch_imgdir"), and the "--bypass" flag indicates that the NutNet defense should be bypassed, thus evaluating the undefended model. The script will output "Recovered/unsuccessful attacks" and "successful attacks": the former refer to the proportion of cases where the patch attack was not effective or properly defended, and the latter to the proportion of cases where the patches were effective and defeated NutNet (the last part is only true if the evaluation does not use the "--bypass" flag). 
+
+IMPORTANT: THIS CODE IS FOCUSED ON OBJ. DETECTION, THUS ONLY INRIA AND VOC SHOULD BE USED.
+
+To get results of the victim model on clean images of the INRIA dataset with visualizations (saved to undefended_clean/):
+```
+python nutnet.py --imgdir data/inria/clean --patch_imgdir data/inria/1p/ --bypass --visualize --clean --savedir undefended_clean/
+```
+This should yield results like the ones in "nutnet_results/".
+
+To get results of the undefended victim model on images of the INRIA dataset with single-patch attacks with visualizations (saved to undefended_1p/):
+```
+python nutnet.py --imgdir data/inria/clean --patch_imgdir data/inria/1p/ --bypass --visualize --savedir undefended_1p/
+```
+
+To get results of the victim model defended by NutNet on images of the INRIA dataset with single-patch attacks with visualizations (saved to nutnet_1p/):
+```
+python nutnet.py --imgdir data/inria/clean --patch_imgdir data/inria/1p/ --visualize --savedir nutnet_1p/
+```
+
---
+++ train_patch.py
@@ -0,0 +1,257 @@
+"""
+Training code for Adversarial patch training
+
+
+"""
+
+import PIL
+import load_data
+from tqdm import tqdm
+import argparse
+from PIL import *
+
+from load_data import *
+import gc
+import matplotlib.pyplot as plt
+from torch import autograd
+from torchvision import transforms
+#from tensorboardX import SummaryWriter
+import subprocess
+from patchFilter import AutoEncoder8 
+#import patch_config
+import os
+import sys
+import time
+
+import torch.fft
+
+class FrequencyLoss(torch.nn.Module):
+    def __init__(self, patch_size, low_freq_ratio=0.25):
+        super(FrequencyLoss, self).__init__()
+        self.patch_size = patch_size
+        self.low_freq_ratio = low_freq_ratio  # e.g., 0.25 means focus on lowest 25% of frequencies
+
+        # Create a circular low-pass mask in the frequency domain
+        yy, xx = torch.meshgrid(torch.arange(patch_size), torch.arange(patch_size), indexing='ij')
+        center = patch_size // 2
+        dist = ((yy - center)**2 + (xx - center)**2).float().sqrt()
+        radius = patch_size * low_freq_ratio
+        self.low_freq_mask = (dist < radius).float()[None, :, :]  # Shape: [1, H, W]
+
+    def forward(self, patch):
+        # patch: [3, H, W]
+        patch_fft = torch.fft.fftshift(torch.fft.fft2(patch, dim=(-2, -1)), dim=(-2, -1))
+        mag = torch.abs(patch_fft)  # [3, H, W]
+        low_freq_energy = (mag * self.low_freq_mask.to(patch.device)).sum()
+        return low_freq_energy / patch.numel()  # normalize
+
+
+def generate_patch(type, n_patches=1, patch_size=300):
+    """
+    Generate a random patch as a starting point for optimization.
+
+    :param type: Can be 'gray' or 'random'. Whether or not generate a gray or a random patch.
+    :return:
+        """
+    if type == 'gray':
+        adv_patch_cpu = torch.full((3, patch_size, patch_size), 0.5)
+    elif type == 'random':
+        adv_patch_cpu = torch.rand((3, patch_size, patch_size))
+
+    return adv_patch_cpu
+
+def train(args):
+    #config = patch_config.patch_configs[mode]()
+    darknet_model = Darknet(args.cfgfile)
+    darknet_model.load_weights(args.weightfile)
+    darknet_model = darknet_model.eval().cuda() # TODO: Why eval?
+    nutnet_ae = AutoEncoder8().cuda().eval()  # Match NutNet settings
+    nutnet_ae.load_state_dict(torch.load('./ae_weights/n_52.pth'))  # Update path if needed
+    nutnet_loss_fn = torch.nn.MSELoss()
+    patch_applier = PatchApplier().cuda()
+    patch_transformer = PatchTransformer(n_patches=args.n_patches).cuda()
+    if args.loss_target=='obj':
+        loss_target=lambda obj, cls: obj
+    elif args.loss_target=='cls':
+        loss_target=lambda obj, cls: cls
+    else:
+        loss_target=lambda obj, cls: obj * cls
+    prob_extractor = MaxProbExtractor(0, 80, loss_target=loss_target).cuda()
+    nps_calculator = NPSCalculator(args.printfile, args.patch_size).cuda()
+    total_variation = TotalVariation().cuda()
+    frequency_loss = FrequencyLoss(args.patch_size, low_freq_ratio=0.25).cuda()
+    n_patches=args.n_patches
+
+    #DATA_DIR=os.path.join('carla_gen_patches/' + str(n_patches) + 'p/')
+    if not os.path.exists(args.savedir):
+        os.makedirs(args.savedir)
+    img_size = darknet_model.height
+    batch_size = args.batch_size
+    n_epochs = args.n_epochs
+    max_lab = 14
+
+    time_str = time.strftime("%Y%m%d-%H%M%S")
+
+    # Generate stating point
+    #Option 1: gray patch
+    if args.patch_checkpoint is None:
+        adv_patch_cpu = generate_patch("gray")
+    #Option 2: some existing image (possibly an adv. patch to be trained some more)
+    else:
+        patch_img_m = PIL.Image.open(args.patch_checkpoint).convert('RGB')
+        tf = transforms.Resize((args.patch_size, args.patch_size))
+        patch_img_m = tf(patch_img_m)
+        tf = transforms.ToTensor()
+        adv_patch_cpu = tf(patch_img_m)
+
+    adv_patch_cpu.requires_grad_(True)
+
+    train_loader = torch.utils.data.DataLoader(InriaDataset(args.imgdir, args.labdir, args.max_lab, img_size,
+                                                            shuffle=False),#True),
+                                               batch_size=batch_size,
+                                               shuffle=False,#True,
+                                               num_workers=1)
+    epoch_length = len(train_loader)
+    print(f'One epoch is {len(train_loader)}')
+
+    optimizer = optim.Adam([adv_patch_cpu], lr=args.start_learning_rate, amsgrad=True)
+    scheduler_factory=lambda x: optim.lr_scheduler.ReduceLROnPlateau(x, 'min', patience=50)
+    scheduler = scheduler_factory(optimizer)
+
+    #initialize the loss and early stopping counter
+    best=np.inf
+    patience=0
+
+    et0 = time.time()
+    for epoch in range(n_epochs):
+        
+        ep_det_loss = 0
+        ep_nps_loss = 0
+        ep_tv_loss = 0
+        ep_loss = 0
+        bt0 = time.time()
+        for i_batch, (img_batch, lab_batch, pth_i) in tqdm(enumerate(train_loader), desc=f'Running epoch {epoch}',
+                                                    total=epoch_length):
+            #if i_batch>0:
+            #    break
+
+            img_batch = img_batch.cuda()
+            lab_batch = lab_batch.cuda()
+            #print('TRAINING EPOCH %i, BATCH %i'%(epoch, i_batch))
+            adv_patch = adv_patch_cpu.cuda()
+            adv_batch_t = patch_transformer(adv_patch, lab_batch, img_size, do_rotate=True, rand_loc=True)
+            p_img_batch = patch_applier(img_batch, adv_batch_t)
+            p_img_batch = F.interpolate(p_img_batch, (darknet_model.height, darknet_model.width))
+
+            #img = p_img_batch[0, :, :,]
+            #img = transforms.ToPILImage()(img.detach().cpu())
+            #img.show()
+
+
+            output, _ = darknet_model(p_img_batch)
+            max_prob = prob_extractor(output)
+            nps = nps_calculator(adv_patch)
+            tv = total_variation(adv_patch)
+            freq = frequency_loss(adv_patch)
+            freq_loss_weight = 1.0  # You can tune this value
+
+
+            nps_loss = nps*0.01
+            tv_loss = tv*2.5
+            det_loss = torch.mean(max_prob)
+            loss = det_loss + nps_loss + torch.max(tv_loss, torch.tensor(0.1).cuda())
+
+            # NutNet-aware patch loss
+            reconstructed_patch = nutnet_ae(adv_patch.unsqueeze(0))  # [1,3,H,W]
+            recon_loss_nutnet = nutnet_loss_fn(reconstructed_patch, adv_patch.unsqueeze(0))
+
+            recon_loss_weight = 5.0  # You can tune this
+            loss = loss + recon_loss_weight * recon_loss_nutnet
+
+            
+
+
+            ep_det_loss += det_loss.detach().cpu().numpy()
+            ep_nps_loss += nps_loss.detach().cpu().numpy()
+            ep_tv_loss += tv_loss.detach().cpu().numpy()
+            ep_loss += loss
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            adv_patch_cpu.data.clamp_(0,1)       #keep patch in image range
+
+            #bt1 = time.time()
+            #bt0 = time.time()
+        et1 = time.time()
+        ep_det_loss = ep_det_loss/len(train_loader)
+        ep_nps_loss = ep_nps_loss/len(train_loader)
+        ep_tv_loss = ep_tv_loss/len(train_loader)
+        ep_loss = ep_loss/len(train_loader)
+        
+
+
+        #im = transforms.ToPILImage('RGB')(adv_patch_cpu)
+        #plt.imshow(im)
+        #plt.savefig(f'pics/{time_str}_{config.patch_name}_{epoch}.png')
+
+        scheduler.step(ep_loss)
+        if (epoch+1)%25==0 or epoch==0 or True:
+            print('  EPOCH NR: ', epoch+1, ' EPOCH LOSS: ', ep_loss, ' IMG NO: ', i_batch)
+            #writer.add_image('patch_adv_{}_{}'.format(config.patch_size, i_batch), adv_patch_cpu, epoch)
+            print('  DET LOSS: ', ep_det_loss)
+            print('  NPS LOSS: ', ep_nps_loss)
+            print('   TV LOSS: ', ep_tv_loss)
+            print('EPOCH TIME: ', et1-et0)
+
+        if ep_loss<best:
+            patience=0
+            best=ep_loss
+            im = transforms.ToPILImage('RGB')(adv_patch_cpu)
+            im.save(args.savedir + '/universal_patch'.format(args.patch_size) +'.png')
+        else:
+            patience=patience+1
+        del adv_batch_t, output, max_prob, det_loss, p_img_batch, nps_loss, tv_loss, loss
+        torch.cuda.empty_cache()
+        et0 = time.time()
+        if patience>=args.patience:
+            print('Early stopping at epoch ', epoch+1)
+            break
+            #joblib.dump(adv_patch.cpu().detach().numpy(), os.path.join(DATA_DIR,'patch_adv_list_{}_{}p_'.format(args.patch_size, args.n_patches) + args.p_split + '.z'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config",default='paper_obj',type=str,help="detection mechanism")
+    parser.add_argument("--savedir",default="testing/",type=str,help="relative directory to save results")
+    parser.add_argument("--cfg",default="cfg/yolo.cfg",type=str,help="relative directory to cfg file")
+    #parser.add_argument("--patchfile",default="patches/object_score.png",type=str,help="relative directory to patch file")
+    parser.add_argument("--weightfile",default="weights/yolo.weights",type=str,help="path to checkpoints")
+    parser.add_argument("--cfgfile",default="cfg/yolo.cfg",type=str,help="path to checkpoints")
+    parser.add_argument('--imgdir', default="inria/Train/pos/clean", type=str,help="path to data")
+    parser.add_argument('--labdir', default="inria/Train/pos/clean/yolo-labels", type=str,help="path to labels")
+    parser.add_argument('--dataset', default='inria', choices=('inria','COCO'),type=str,help="dataset")
+
+    parser.add_argument("--model",default='resnet50',type=str,help="model name")
+    parser.add_argument("--patch_checkpoint",default=None,type=str,help="file to start training from")
+    parser.add_argument("--clip",default=-1,type=int,help="clipping value; do clipping when this argument is set to positive")
+    parser.add_argument("--aggr",default='mean',type=str,help="aggregation methods. set to none for local feature")
+    parser.add_argument("--loss_target",default='obj',type=str,help="patch attack objective")
+    parser.add_argument("--batch_size", default=16, type=int, help="betch size")
+    parser.add_argument("--patch_size", default=300, type=int, help="size of the adversarial patch")
+    parser.add_argument("--max_lab", default=14, type=int, help="size of the adversarial patch")
+    parser.add_argument("--patience", default=10, type=int, help="epochs with no improvement for early stopping")
+    parser.add_argument("--n_epochs", default=20000, type=int, help="size of the adversarial patch")
+
+    parser.add_argument("--fix",action='store_true',help="hold patch size")
+    parser.add_argument("--n_patches",default=1,type=int,help="number of patches")
+    parser.add_argument("--start_learning_rate",default=0.03,type=float,help="initial learning rate for optimizer")
+    parser.add_argument('--printfile', default="non_printability/30values.txt", type=str,help="path to printability file")
+
+
+    parser.add_argument("--p_split",default='keep',type=str,help="how patchsize is distributed to n>1 patches (keep or red)")
+    parser.add_argument("--skip",default=1,type=int,help="number of example to skip")
+    parser.add_argument("--save",action='store_true',help="save results to txt")
+    args = parser.parse_args()
+    train(args=args)
+    #trainer = PatchTrainer(args=args, n_patches=args.n_patches)
+    #trainer.train()
